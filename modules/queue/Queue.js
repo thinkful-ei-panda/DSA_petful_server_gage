372b4c25537d7b18635f6fcb88e630a5d6e9a7f2
Gage <gagemigan@gmail.com>
2020-08-28 10:39:27 -0400

    added Queue TODO add show & all

--- modules/queue/Queue.js
+++ modules/queue/Queue.js
@@ -1,16 +1,40 @@
+class _Node {
+  constructor (value){
+    this.value = value;
+    this.next = null;
+  }
+}
 class Queue {
   constructor() {
-    // Set initial data.
+    this.first = null;
+    this.last = null;
   }
-
   enqueue(data) {
-    // Add some data to the queue.
-  }
+    const node = new _Node(data);
 
+    if (this.first === null){
+      this.first = node;
+    }
+
+    if (this.last) {
+      this.last.next = node;
+    }
+    //make the new node the last item on the queue
+    this.last = node;
+  }
   dequeue() {
-    // Remove some data from the queue.
+    if (this.first === null) {
+      return;
+    }
+    const node = this.first;
+    this.first = this.first.next;
+    //if this is  the last item in the queue
+    if (node === this.last) {
+      this.last = null;
+    }
+    return node.value;
   }
-
+  
   show() {
     // Return the next item in the queue.
   }
@@ -20,4 +44,4 @@ class Queue {
   }
 }
 
-module.exports = Queue
+module.exports = Queue;
